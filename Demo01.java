e6fc49c0e14803e71aa6d44c7a8dfcef3b87fe57
zhansan <123@qq.com>
2019-02-22 19:13:56 +0800

    保存至什么区

--- Demo01.java
+++ Demo01.java
@@ -3,6 +3,15 @@ package cn.itcast;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("demo执行了方法并保存到缓存区");
+        System.out.println("demo执行了方法并保存到缓存区");
+        System.out.println("demo执行了方法并保存到缓存区");
+        System.out.println("demo执行了方法并保存到缓存区");
+        System.out.println("demo执行了方法并保存到缓存区");
+        System.out.println("demo执行了方法并保存到缓存区");
+        System.out.println("demo执行了方法并保存到缓存区");
+        System.out.println("demo执行了方法并保存到缓存区");
+        System.out.println("demo执行了方法并保存到缓存区");
+        System.out.println("demo执行了方法并保存到缓存区");
     }
 
 }
